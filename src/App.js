ccebe55ca85d833ccd8946474d64ea3651461b5b
Austin Varga <austinvarga1@protonmail.com>
2023-01-05 09:22:19 -0800

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Header';
+import Landing from './Landing';
+import Projects from './Projects';
+import Contact from './Contact';
 import './App.css';
+
+const projects = [
+  {
+    title: 'Project 1',
+    image: '/path/to/project1.jpg',
+    description: 'This is a description of project 1',
+    link: '/project1'
+  },
+  {
+    title: 'Project 2',
+    image: '/path/to/project2.jpg',
+    description: 'This is a description of project 2',
+    link: '/project2'
+  },
+  // add more projects here
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <Landing />
+      <Projects projects={projects} />
+      <Contact />
+    </React.Fragment>
   );
 }
 
